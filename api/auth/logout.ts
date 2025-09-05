865b1c5fe626c60d3f796628d06f7fac56e88117
lmbsumatra <122442812+lmbsumatra@users.noreply.github.com>
2025-09-05 15:22:37 +0800

    :: added logout

---
+++ api/auth/logout.ts
@@ -0,0 +1,32 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import dotenv from "dotenv";
+import cookie from "cookie";
+
+dotenv.config();
+
+const handler = async (req: VercelRequest, res: VercelResponse) => {
+    try {
+        res.setHeader("Set-Cookie", [
+            cookie.serialize("accessToken", "", {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === "production",
+                sameSite: "strict",
+                path: "/",
+                expires: new Date(0), // clear immediately
+            }),
+            cookie.serialize("refreshToken", "", {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === "production",
+                sameSite: "strict",
+                path: "/",
+                expires: new Date(0), // clear immediately
+            }),
+        ]);
+
+        return res.status(200).json({ message: "Log out successful!" });
+    } catch (error: any) {
+        return res.status(500).json({ error: error.message });
+    }
+};
+
+export default handler;
